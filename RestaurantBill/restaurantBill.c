29de5b59e7e4c6ecab51101a757ef91d73d23158
Keana De Padua <ksdepadua@cpp.edu>
2026-03-06 13:54:39 -0800

    Initial creation of RestaurantBill and restaurant_bill.c; Initial outline of program

---
+++ RestaurantBill/restaurantBill.c
@@ -0,0 +1,40 @@
+/*  
+    KEANA DE PADUA - QUIZ 1
+    QUESTION 1, PART A 
+*/
+
+// Include the stdio library
+
+// Declare enum meals{salad, soup, sandwich, pizza} (0 - 3)
+
+// Declare functions
+
+/* START OF main() FUNCTION */
+
+// Declare 2 floats to hold tax and tip percentages 
+//  (assuming 0.XX format (no more that 2 places after the decimal point))
+// Declare an int meal
+//  Assign a value from pick_meal()
+// Call print_meal()
+
+/* END OF main() FUNCTION */
+
+/* START OF pick_meal() FUNCTION */
+
+// Declare a random int within the range (0 - 3) (inclusive) that represents
+//  the meal that's chosen.
+// Return random integer
+
+/* END OF pick_meal() FUNCTION*/
+
+/* START OF print_meal() FUNCTION */
+
+// Begin switch statement to determine which meal was picked
+
+// Print meal name
+// Print meal cost
+// Print tax amount
+// Print tip amount
+// Print total bill
+
+/* END OF print_meal() FUNCTION*/
