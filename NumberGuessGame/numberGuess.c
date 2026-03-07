d494b9a6ee56f7497479889051c80f3a44bb5494
Keana De Padua <ksdepadua@cpp.edu>
2026-03-06 23:20:30 -0800

    Problem 2 finished

--- NumberGuessGame/numberGuess.c
+++ NumberGuessGame/numberGuess.c
@@ -1,84 +1,155 @@
 /*
     KEANA DE PADUA - QUIZ 1
     QUESTION 2
+
+    Note:   Unfortunately, there are errors with the code that I could not resolve. If you don't mind, professor, I would appreciate any 
+            feedback you have for this program. I apologize for the messy code and lack of desired output. Listed below are the issues 
+            with my program (at least the ones I've been able to catch).
+            -   Since I am reading in user input as a char to try and detect a possible 'q' input, then converting the char to an int if it isn't 'q',
+                it doesn't allow me to read in any guesses that are more than one digit (only reads 1-9 correctly). I have tried doing vice versa, 
+                since I know chars can be stored as ints (in ASCII form), but I don't seem to get the correct output. This was the best I was able
+                to come up with.
+                -   I was able to verify that option 2 (changing the max number) does indeed work through setting breakpoints and checking the actual
+                    values, but since my program can't even handle numbers > 9, there's not really any other way of showing it in the output.
+
+
 */
 
-//  Include needed libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-//  Create enums
-//      gameResult{lost, won}
+enum gameResult{lost, won}; // Create enums
 
-// Declare functions
+// declare functions
+static int getMenuChoice();
+static char getGameInput();
+static int getUserMax();
 
-/*  START main() FUNCTION */
+int main(int argc, char *argv[]){
+    int currentNumGuesses = 0, currentGame = 0;
+    int gameMax = 10;
+    int userMenuChoice = getMenuChoice();
+    int games[10][2];
+//      This is assuming that the max amount of games a player will play is 10 games...
+//      1st column = Whether that game was won or lost (1 or 2)
+//      2nd column = Number of guesses made until the end of the game (so either until the player won or quit)
 
-//  Declare + assign int currentNumGuesses = 0, currentGame = 0
-//  Declare int 2d array games[] w/ 2 rows, 50 colums
-//      This is assuming that the max amount of games a player will play is 50 games...
-//      1st row = Whether that game was won or lost (1 or 2)
-//      2nd row = Number of guesses made until the end of the game (so either until the player won or quit)
+    while(userMenuChoice != 3){
+        switch(userMenuChoice){
+            case 1:
+                srand(time(0));
+                int gameVal = (rand() % gameMax) + 1;
+                int currentNumGuesses = 0;
+                int userGameInputInt;
+                
+                char userGameInputChar = getGameInput();
+                if(userGameInputChar != 'q')
+                    userGameInputInt = atoi(&userGameInputChar);
+                
+                currentNumGuesses++;
+                
+                while(userGameInputInt != gameVal && userGameInputChar != 'q') {   
+                    if(userGameInputInt > gameVal)
+                        printf("Your guess was too high. \n");
+                    else 
+                        printf("Your guess was too low.\n");
+            
+                    userGameInputChar = getGameInput();
+                    if(userGameInputChar != 'q')
+                        userGameInputInt = atoi(&userGameInputChar);
+            
+                    currentNumGuesses++;
+                }
+        
+                if(userGameInputInt == gameVal) {
+                    printf("You won!\n");
+                    games[currentGame][0] = won;
+                }
+                else if(userGameInputChar == 'q') {
+                    printf("You lost.\n");
+                    games[currentGame][0] = lost;
+                }
 
-//  START WHILE LOOP
-//      Declare and assign int gameMax = 10;
-//      Declare int variable for userMenuChoice
-//
-//      Call getMenuChoice()
-//
-//      START SWITCH STATEMENT
-//          Case 1:
-//              Call playGame()
-//              Break out of switch statement
-//          Case 2:
-//              Assign gameMax = getUserMax();
-//              Break out of switch statement
-//          Default:
-//              Break out of switch statement
-//      END SWITCH STATEMENT
-//      Break out of loop
-//  END WHILE LOOP
+                games[currentGame][1] = currentNumGuesses;
 
-//  Call printEnd()
+                currentGame++;
 
-/*  END main() FUNCTION */
+                break;
 
-/*  START getMenuChoice() FUNCTION */
-//      Declare int for userInput
-//      Print menu
-//      Prompt user for input
-//      Return user input
-/*  END getMenuChoice() FUNCTION */
+            case 2:
+                gameMax = getUserMax();
+                break;
 
-/*  START getGameInput() FUNCTION */
-//      Declare int for userInput
-//      Prompt user for input
-//      Return user input
-/*  END getGameInput() FUNCTION */
+            default:
+                break;
+        }   
+        userMenuChoice = getMenuChoice();
+    }
 
-/* START playGame() FUNCTION */
-//  Declare and assign gameVal = (rand() % gameMax) + 1;
-//  Declare and assign int variable for userGameInput = getGameInput()
-//  Increment currentNumGuesses
-//      While user is wrong AND userGameInput != 'q'...
-//          Tell them they were too low/high
-//          Call getGameInput()
-//          Increment currentNumGuesses
-//          If user is right OR userGameInput == 'q'
-//              Break out of loop
-//      If user is correct
-//          Tell them they won
-//          Count game as a win
-//      Else (User enters 'q')
-//          Count game as a loss
-//      Increment currentGame
-/* END playGame() FUNCTION */
+    printf("Thank you for playing!\n");
+    int i;
+    for(i = 0; i < currentGame; i++) {
+        int temp = i;
+        printf("\nGAME %d", temp + 1);
+        printf(":\n");
 
-/*  START getUserMax() FUNCTION */
-//      Tell the user the max value they can enter
-//      Validate input is NOT a negative number or above the max value
-/*  END getUserMax() FUNCTION*/
+        printf("Result: ");
+        if(games[i][0] == won)
+            printf("Game won!\n");
+        else if(games[i][0] == lost)
+            printf("Game lost.\n");
+        
+        printf("Guesses made: %d", games[i][1]);
+    }
 
-/* START printEnd() FUNCTION */
-//  Thank user for playing
-//  Print out the result of all games played + number of guesses required to win
-//  End the game/program
-/* END printEnd() FUNCTION */
+    // Saving the current max to a file
+    FILE* fptr;
+
+    fptr = fopen("save_user_max_number.txt", "w");     // Will open a new file or overwrite existing one
+    if(fptr != NULL){
+        char str[10];
+        sprintf(str, "%d", gameMax);
+        fputs(str, fptr);
+    }
+
+    return 0;
+}
+
+static int getMenuChoice()
+{
+    int userInput;
+    printf("--- MENU ---\n");
+    printf("Press 1 to play a game\n");
+    printf("Press 2 to change the max number\n");
+    printf("Press 3 to quit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &userInput);
+    
+    return userInput;
+}
+
+static char getGameInput() {
+    char userInputChar;
+    printf("Input your guess: ");
+    scanf("%c", &userInputChar);    // clear buffer from prev newline
+    scanf("%c", &userInputChar);
+    return userInputChar;
+}
+
+static int getUserMax() {
+    int max;
+    printf("The maximum value you can pick is %d\n", INT_MAX);
+    printf("Enter the max: ");
+    scanf("%d", &max);
+    
+    while(max < 0 || max > INT_MAX) {
+        printf("Invalid max\n");
+        printf("Enter the max: ");
+        scanf("%d", &max);
+    }
+
+    printf("The new max is: %d\n", max);
+
+    return max;
+}
